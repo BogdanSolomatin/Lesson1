1414ea5571ab6d08ffbf64a834836462019e6cd7
Bogdan Solomatin <bundesgott@gmail.com>
2020-11-15 16:44:42 +0500

    Lesson 4 tasks

---
+++ Capitalise.js
@@ -0,0 +1,3 @@
+let capitalise = str => str.charAt(0).toUpperCase() + str.substring(1,str.length);
+
+alert(capitalise(prompt('Input str to update')));
---
+++ GetDigit.js
@@ -0,0 +1,10 @@
+let getDigitSum = value => {
+    let sum = 0;
+    while(value > 0) {
+        sum += value % 10;
+        value = Math.floor(value/10);
+    }
+    return Math.floor(sum);
+}
+
+alert(getDigitSum(parseInt(prompt('Input value'))));
