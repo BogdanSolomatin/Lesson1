64f4a7de49b1e139d5c1af22708214e77bb4c9dc
Bogdan Solomatin <bundesgott@gmail.com>
2020-11-18 16:53:51 +0500

    Add message if number of attemps is zero

--- GuessTheNumber.js
+++ GuessTheNumber.js
@@ -2,11 +2,14 @@ const DIGIT = Math.floor(Math.random() * (10 - 1) + 1);
 
 let limit = 5;
 let flag = false;
+
 do {
     limit--;
     flag = check(prompt('Input the number'),right,more,less);
     if(flag) break;
 } while (limit > 0);
+
+if(limit == 0)  alert('You have not guessed the number for a given number of attempts');
 
 function check (ans, rightAns, moreThan, lessThan) {
     return ans == DIGIT ? rightAns() : ans > DIGIT ? moreThan() : lessThan();
